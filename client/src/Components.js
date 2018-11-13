06ae4dbf50e84be04ece12259e68b5f4b219fab3
Terrance Li <terranceli.terbear@gmail.com>
2018-11-13 15:21:43 -0800

    Modified package.json and logo to reflect name change

--- client/src/Components.js
+++ client/src/Components.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export class Logo extends Component {
 	render() {
-		return <h2 className="logo">egyptian ratscrew</h2>
+		return <h2 className="logo">hearts</h2>
 	}
 }
 
